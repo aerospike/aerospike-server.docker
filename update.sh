8921bf5b33462ebadcf3cfe425aa5bcddb1868b8
Joe Ferguson <joe@infosiftr.com>
2015-01-30 14:54:47 -0800

    Add update script to check for new versions

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+fullVersion="$(curl -sSL 'https://www.aerospike.com/artifacts/aerospike-server-community/' | grep -E '<a href="[0-9.-]+/"' | sed -r 's!.*<a href="([0-9.-]+)/".*!\1!' | sort -V | tail -1)"
+
+sha256="$(curl -sSL "https://www.aerospike.com/artifacts/aerospike-server-community/${fullVersion}/aerospike-server-community-${fullVersion}-debian7.tgz.sha256" | cut -d' ' -f1)"
+
+set -x
+sed -ri '
+	s/^(ENV AEROSPIKE_VERSION) .*/\1 '"$fullVersion"'/;
+	s/^(ENV AEROSPIKE_SHA256) .*/\1 '"$sha256"'/;
+' Dockerfile
