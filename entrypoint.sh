9070e021d82dc4946cbd3b5adfd7ad8505951c5f
Lucien Volmar <lucien@aerospike.com>
2016-11-03 11:28:41 -0700

    3.10.0-3a

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 set -e
-# adding sleep to wait for network interfaces to be available
-sleep 12
+NETLINK=${NETLINK:-eth0}
+
+
+# we will wait a bit for the network link to be up.
+NETLINK_UP=0
+NETLINK_COUNT=0
+echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate)"
+while [ $NETLINK_UP -eq 0 ] && [ $NETLINK_COUNT -lt 20 ]; do
+	cat /sys/class/net/${NETLINK}/operstate | grep -q "up"
+	if [ $? -eq 0 ]; then
+		NETLINK_UP=1
+	else
+		sleep 0.1
+		let NETLINK_COUNT=NETLINK_COUNT+1
+	fi
+done
+echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate) in ${NETLINK_COUNT}"
+
+
+
 # if command starts with an option, prepend asd
 if [ "${1:0:1}" = '-' ]; then
 	set -- asd "$@"
