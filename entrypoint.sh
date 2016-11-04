33a4dade9e13ebd34c7bc879d491ce6904bc52fa
Lucien Volmar <lucien@aerospike.com>
2016-11-04 13:30:47 -0700

    3.10.0-3b

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 set -e
-NETLINK=${NETLINK:-eth0}
-
-
-# we will wait a bit for the network link to be up.
-NETLINK_UP=0
-NETLINK_COUNT=0
-echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate)"
-while [ $NETLINK_UP -eq 0 ] && [ $NETLINK_COUNT -lt 20 ]; do
-	cat /sys/class/net/${NETLINK}/operstate | grep -q "up"
-	if [ $? -eq 0 ]; then
-		NETLINK_UP=1
-	else
-		sleep 0.1
-		let NETLINK_COUNT=NETLINK_COUNT+1
-	fi
-done
-echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate) in ${NETLINK_COUNT}"
-
 
 
 # if command starts with an option, prepend asd
@@ -27,6 +9,24 @@ fi
 
 # if asd is specified for the command, start it with any given options
 if [ "$1" = 'asd' ]; then
+
+	NETLINK=${NETLINK:-eth0}
+
+
+	# we will wait a bit for the network link to be up.
+	NETLINK_UP=0
+	NETLINK_COUNT=0
+	echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate)"
+	while [ $NETLINK_UP -eq 0 ] && [ $NETLINK_COUNT -lt 20 ]; do
+		if grep -q "up" /sys/class/net/${NETLINK}/operstate; then
+	                NETLINK_UP=1
+	        else
+	                sleep 0.1
+                	let NETLINK_COUNT=NETLINK_COUNT+1
+        	fi
+	done
+	echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate) in ${NETLINK_COUNT}"
+
 	# asd should always run in the foreground
 	set -- "$@" --foreground
 
