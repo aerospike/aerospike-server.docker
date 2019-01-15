8ba5e9490fb4c857d4fc7e3a904326f1d5b57863
Shrihari Rathod <11029762+shriharirathod@users.noreply.github.com>
2019-01-15 13:13:57 +0530

    Sandbox fixes (#10)
    * DP-1934: Custom port for REST
    
    Signed-off-by: Shrihari Rathod <shrihari.rathod@primaryio.com>
    
    * DP-1908: Adding memory size and disks
    
    Adding memory size and disks as config args
    
    Signed-off-by: Shrihari Rathod <shrihari.rathod@primaryio.com>
    
    * DP-1908: Parsing new args
    
    Signed-off-by: Shrihari Rathod <shrihari.rathod@primaryio.com>
    
    * Update config value to avoid overcommit
    
    Signed-off-by: Shrihari Rathod <shrihari.rathod@primaryio.com>
    
    * Fixed issue while parsing disks
    
    Signed-off-by: Shrihari Rathod <shrihari.rathod@primaryio.com>
    
    * Calculating memory dynamically
    
        Calculating the memory requirements for aerospike
        dynamically keeping in account excess available memory.
    
    Signed-off-by: Shrihari Rathod <shrihari.rathod@primaryio.com>
    
    * Updating sizing calculations
    
    Signed-off-by: Shrihari Rathod <shrihari.rathod@primaryio.com>
    
    * Fixed typo
    
    Signed-off-by: Shrihari Rathod <shrihari.rathod@primaryio.com>

--- src/hyc_asd_mgr.py
+++ src/hyc_asd_mgr.py
@@ -34,10 +34,26 @@ MULTICAST_CONFIG_FILE  = "/etc/aerospike/aerospike_multicast.conf"
 MODDED_FILE            = "/etc/aerospike/modded.conf"
 FILE_IN_USE            = None
 
-class ComponentStop(object):
+#
+# String Markers inserted in config file
+#
+CLEAN_DISKS_MARKER = "CLEAN_DISKS"
+DIRTY_DISKS_MARKER = "DIRTY_DISKS"
+MEMORY_PER_NS_MARKER = "MEMORY_PER_NS"
+MWC_MEMORY_MARKER = "MWC_MEMORY"
+PWQ_MEMORY_MARKER = "PWQ_MEMORY"
 
-    def on_get(self, req, resp):
-        resp.status = HTTP_OK
+config_high = { "max-write-cache" : 536870912,
+                "post-write-queue" : 2048,
+                "memory_per_ns" : 10,
+                "system" : 4
+            }
+
+config_low  = { "max-write-cache" : 268435456,
+                "post-write-queue" : 1024,
+                "memory_per_ns" : 2,
+                "system" : 1,
+            }
 
 def is_service_up():
     cmd = "pidof asd"
@@ -51,7 +67,59 @@ def is_service_up():
 
     return True
 
-def create_mesh_config(mesh_addrs, mesh_port):
+def get_memory_config(memory, disks):
+
+    log.debug("Total: %s" %memory)
+    if memory >= 10 and memory < 20:
+        config = config_low
+
+    elif memory >= 20:
+        config = config_high
+
+    min_req    = disks * (config['max-write-cache'] >> 20) / 1024
+    min_req    = min_req + disks * (config['post-write-queue'] >> 10) + config['system']
+    avail_mem  = memory - min_req
+
+    log.debug("min_req: %s avail_mem: %s" %(min_req, avail_mem))
+    if avail_mem > 2:
+        config['memory_per_ns'] = avail_mem / 2
+        log.debug("memory_per_ns: %s" %config['memory_per_ns'])
+
+    return config
+
+def get_disks_for_config(no_disks):
+    disks = ['sdb', 'sdc', 'sdd', 'sde', 'sdf', 'sdg', 'sdh', 'sdi', 'sdj', 'sdk']
+    dev_str = "\t\tdevice /dev/"
+
+    no_disks = int(no_disks)
+
+    disks_to_use = disks[:no_disks]
+    clean = disks_to_use[:(no_disks/2)]
+    dirty = disks_to_use[(no_disks/2):]
+
+    clean_str = dirty_str = ""
+    for d in clean:
+        tmp_str = dev_str + d + "\n"
+        clean_str = clean_str + tmp_str
+
+    for d in dirty:
+        tmp_str = dev_str + d + "\n"
+        dirty_str = dirty_str + tmp_str
+
+    return (clean_str, dirty_str)
+
+def create_mesh_config(mesh_addrs, mesh_port, memory, disks):
+
+    if not memory:
+        log.debug("Memory not set using default")
+        memory = 10
+    mem_config = get_memory_config(int(memory), int(disks))
+
+    if not disks:
+        log/debug("Disk not set using default")
+        disks = 2
+    (clean_str, dirty_str) = get_disks_for_config(disks)
+
     with open(MESH_CONFIG_FILE, 'r') as input_file, open(MODDED_FILE, 'w+') as output_file:
         filedata = input_file.readlines()
 
@@ -76,6 +144,29 @@ def create_mesh_config(mesh_addrs, mesh_port):
                 update_port = 0
                 continue
 
+            elif line.strip().startswith(CLEAN_DISKS_MARKER):
+                output_file.write(clean_str)
+                continue
+
+            elif line.strip().startswith(DIRTY_DISKS_MARKER):
+                output_file.write(dirty_str)
+                continue
+
+            elif line.strip().startswith(MEMORY_PER_NS_MARKER):
+                new_str = "\tmemory-size %sG\n" %mem_config["memory_per_ns"]
+                output_file.write(new_str)
+                continue
+
+            elif line.strip().startswith(MWC_MEMORY_MARKER):
+                new_str = "\t\tmax-write-cache %s\n" %mem_config["max-write-cache"]
+                output_file.write(new_str)
+                continue
+
+            elif line.strip().startswith(PWQ_MEMORY_MARKER):
+                new_str = "\t\tpost-write-queue %s\n" %mem_config["post-write-queue"]
+                output_file.write(new_str)
+                continue
+
             else:
                 output_file.write(filedata[n])
                 continue
@@ -83,7 +174,17 @@ def create_mesh_config(mesh_addrs, mesh_port):
     log.debug("Mesh config created")
     return
 
-def create_multicast_config(multi_addr, multi_port):
+def create_multicast_config(multi_addr, multi_port, memory, disks):
+    if not memory:
+        log.debug("Memory not set using default")
+        memory = 10
+    mem_config = get_memory_config(int(memory), int(disks))
+
+    if not disks:
+        log.debug("Disk not set using default")
+        disks = 2
+    (clean_str, dirty_str) = get_disks_for_config(disks)
+
     with open(MULTICAST_CONFIG_FILE, 'r') as input_file, open(MODDED_FILE, 'w+') as output_file:
         filedata = input_file.readlines()
 
@@ -101,6 +202,24 @@ def create_multicast_config(multi_addr, multi_port):
                 filedata[n] = "\t\tport %s\n" %multi_port
                 output_file.write(filedata[n])
                 update_port = 0
+                continue
+            elif line.strip().startswith(CLEAN_DISKS_MARKER):
+                output_file.write(clean_str)
+                continue
+            elif line.strip().startswith(DIRTY_DISKS_MARKER):
+                output_file.write(dirty_str)
+                continue
+            elif line.strip().startswith(MEMORY_PER_NS_MARKER):
+                new_str = "\tmemory-size %sG\n" %mem_config["memory_per_ns"]
+                output_file.write(new_str)
+                continue
+            elif line.strip().startswith(MWC_MEMORY_MARKER):
+                new_str = "\t\tmax-write-cache %s\n" %mem_config["max-write-cache"]
+                output_file.write(new_str)
+                continue
+            elif line.strip().startswith(PWQ_MEMORY_MARKER):
+                new_str = "\t\tpost-write-queue %s\n" %mem_config["post-write-queue"]
+                output_file.write(new_str)
                 continue
             else:
                 output_file.write(filedata[n])
@@ -247,6 +366,12 @@ class ComponentMgr(Thread):
         self.halib.set_health(False)
         return
 
+class ComponentStop(object):
+
+    def on_get(self, req, resp):
+        resp.status = HTTP_OK
+
+
 services = {
 	'register_udf': RegisterUDF(),
 	'unregister_udf': UnRegisterUDF(),
@@ -254,7 +379,7 @@ services = {
        }
 
 
-args = ["etcdip", "svc_label", "svc_idx", "mode","ip", "port"]
+args = ["etcdip", "svc_label", "svc_idx", "mode","ip", "port", "memory", "disks"]
 
 etcd_server_ip = None
 service_type   = None
@@ -262,6 +387,8 @@ service_idx    = None
 mode           = None
 ip_addr        = None
 port_to_use    = None
+memory         = None
+disks          = None
 
 for arg in sys.argv:
     if arg.startswith("etcdip"):
@@ -288,21 +415,29 @@ for arg in sys.argv:
         port_to_use = arg.split("=")[1]
         continue
 
+    elif arg.startswith("memory"):
+        memory = arg.split("=")[1]
+        continue
+
+    elif arg.startswith("disks"):
+        disks = arg.split("=")[1]
+        continue
 
 if mode != '':
     FILE_IN_USE = MODDED_FILE
     if mode == 'mesh':
-        create_mesh_config(ip_addr, port_to_use)
+        create_mesh_config(ip_addr, port_to_use, memory, disks)
 
     elif mode == 'multicast':
-        create_multicast_config(ip_addr, port_to_use)
+        create_multicast_config(ip_addr, port_to_use, memory, disks)
 
 if etcd_server_ip == '' and service_type == '' and service_idx == '':
     etcd_server_ip = "127.0.0.1"
     service_type   = "AS_Server"
     service_idx    = 1
 
-print (etcd_server_ip, service_type, service_idx, mode, ip_addr, port_to_use)
+print (etcd_server_ip, service_type, service_idx, mode, ip_addr, port_to_use,
+        memory, disks)
 
 # Creating AsdManager instance
 component_mgr  = ComponentMgr(etcd_server_ip, service_type, service_idx, VERSION)
--- start.sh
+++ start.sh
@@ -1,3 +1,88 @@
 #!/bin/sh
 
-gunicorn -b 0.0.0.0:8000 hyc_asd_mgr:app etcdip=$1 svc_label=$2 svc_idx=$3 mode=$4 ip=$5 port=$6
+echo "ASD package start"
+OPTIONS=e:l:n:m:i:p:h:y:d:
+LONGOPTS=etcdip:,svc_label:,svc_idx:,mode:,ip:,port:,ha_port:,memory_size_gb:,disks:
+
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+eval set -- "$PARSED"
+
+ETCDIP=""
+SVCLABEL=""
+SVCIDX=1
+MODE=""
+IP=""
+PORT=""
+HA_PORT=8000
+MEMORY=10
+DISKS=2
+
+while true ; do
+      case $1 in
+            -e|--etcdip)
+                  echo "Setting etcd ip to [$2] !"
+                  ETCDIP=$2
+                  shift 2
+                  ;;
+
+            -l|--svc_label)
+                  echo "Setting svc_label to [$2] !"
+                  SVCLABEL=$2
+                  shift 2
+                  ;;
+
+            -n|--svc_idx)
+                  echo "Setting svc_idx to [$2] !"
+                  SVCIDX=$2
+                  shift 2
+                  ;;
+
+            -m|--mode)
+                  echo "Setting mode to [$2] !"
+                  MODE=$2
+                  shift 2
+                  ;;
+
+            -i|--ip)
+                  echo "Setting ip to [$2] !"
+                  IP=$2
+                  shift 2
+                  ;;
+
+            -p|--port)
+                  echo "Setting port to [$2] !"
+                  PORT=$2
+                  shift 2
+                  ;;
+
+            -h|--ha_port)
+                  echo "Setting ha_port to [$2] !"
+                  HA_PORT=$2
+                  shift 2
+                  ;;
+
+            -y|--memory_size_gb)
+                  echo "Setting memory to [$2] !"
+                  MEMORY=$2
+                  shift 2
+                  ;;
+
+            -d|--disks)
+                  echo "Setting disks to [$2] !"
+                  DISKS=$2
+                  shift 2
+                  ;;
+
+            --)
+                  shift
+                  break
+                  ;;
+
+            *)
+                  echo "Programming error"
+                  exit 3
+                  ;;
+      esac
+done
+
+gunicorn -b 0.0.0.0:$HA_PORT hyc_asd_mgr:app etcdip=$ETCDIP svc_label=$SVCLABEL svc_idx=$SVCIDX mode=$MODE ip=$IP port=$PORT memory=$MEMORY disks=$DISKS
