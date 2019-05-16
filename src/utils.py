52569fdf4ddb4d5d2e8676dcd5363473c690d213
harishpujari <harish.pujari@primaryio.com>
2019-05-16 20:07:56 +0530

    log_improvements: Added google log format and log rotation.

--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,19 @@ import os
 import json
 import shutil
 import subprocess
+import glog as log
+from glog import GlogFormatter
 import logging
+import logging.handlers
 
-FORMAT = '%(name)s %(asctime)-15s - %(levelname)s -%(message)s'
-logging.basicConfig(format=FORMAT, filename="/var/log/aerospike/hyc_asd_mgr.log",
-                filemode='a', level=logging.DEBUG)
-log = logging
+def hdm_log_setup(log_file, log_level):
+    fh = logging.handlers.RotatingFileHandler(
+        filename=log_file, mode='a', maxBytes=10<<20, backupCount=5)
+    fh.setFormatter(GlogFormatter())
+    log.logger.addHandler(fh)
+    log.setLevel(log_level)
+
+hdm_log_setup('/var/log/aerospike/hyc_asd_mgr.log', logging.DEBUG)
 
 def dump_data(payload):
 
