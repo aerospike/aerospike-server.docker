569f698dc59480e81976b409335ec4791759456b
Kevin Porter <kporter@aerospike.com>
2023-10-19 10:44:09 -0700

    Cleanup 'bake.hcl' generation code. (#54)

--- update.sh
+++ update.sh
@@ -7,103 +7,14 @@ source lib/log.sh
 source lib/support.sh
 source lib/version.sh
 
-function run_template() {
-	local distro=$1
-	local edition=$2
-
-	log_info "distro '${distro}' edition '${edition}'"
-
-	local server_version="${g_server_version}"
-	local tools_version="${g_tools_version}"
-
-	if [ -z "${tools_version}" ]; then
-		tools_version=$(find_latest_tools_version_for_server "${distro}" "${edition}" "${server_version}")
-	fi
-
-	DEBUG="${DEBUG:=false}"
-	LINUX_BASE="$(support_distro_to_base "${distro}")"
-	AEROSPIKE_VERSION="${server_version}"
-	CONTAINER_RELEASE="${g_container_release}"
-	AEROSPIKE_EDITION="${edition}"
-	AEROSPIKE_DESCRIPTION="Aerospike is a real-time database with predictable performance at petabyte scale with microsecond latency over billions of transactions."
-	AEROSPIKE_X86_64_LINK="$(get_package_link "${distro}" "${edition}" "${server_version}" "${tools_version}" "x86_64")"
-	AEROSPIKE_SHA_X86_64="$(fetch_package_sha "${distro}" "${edition}" "${server_version}" "${tools_version}" "x86_64")"
-	AEROSPIKE_AARCH64_LINK="$(get_package_link "${distro}" "${edition}" "${server_version}" "${tools_version}" "aarch64")"
-	AEROSPIKE_SHA_AARCH64="$(fetch_package_sha "${distro}" "${edition}" "${server_version}" "${tools_version}" "aarch64")"
-
-	log_info "DEBUG: '${DEBUG}'"
-	log_info "LINUX_BASE: '${LINUX_BASE}'"
-	log_info "AEROSPIKE_VERSION: '${AEROSPIKE_VERSION}'"
-	log_info "CONTAINER_RELEASE: '${CONTAINER_RELEASE}'"
-	log_info "AEROSPIKE_EDITION: '${AEROSPIKE_EDITION}'"
-	log_info "AEROSPIKE_DESCRIPTION: '${AEROSPIKE_DESCRIPTION}'"
-	log_info "AEROSPIKE_X86_64_LINK: '${AEROSPIKE_X86_64_LINK}'"
-	log_info "AEROSPIKE_SHA_X86_64: '${AEROSPIKE_SHA_X86_64}'"
-	log_info "AEROSPIKE_AARCH64_LINK: '${AEROSPIKE_AARCH64_LINK}'"
-	log_info "AEROSPIKE_SHA_AARCH64: '${AEROSPIKE_SHA_AARCH64}'"
-
-	local target_path="${edition}/${distro}"
-
-	copy_template "${target_path}"
-	bash_eval_templates "${target_path}"
-
-	# --------------- Print the targets for Docker Buildx Bake ---------------------------
-	local platform_list
-	IFS=' ' read -r -a platform_list <<<"$(support_platforms_for_asd "${server_version}" "${edition}")"
-
-	for platform in "${platform_list[@]}"; do
-		log_info "------ Baking target for edition=${edition} distro=${distro} platform=${platform}----"
-		local short_platform=${platform#*/}
-		local target_str="${edition}_${distro}_${short_platform}"
-		g_test_group_str+="\"${target_str}\", "
-
-		local temp_str="target \"${target_str}\" {\n"
-		temp_str+="\t tags=[\"aerospike/aerospike-server-${edition}-${short_platform}:${server_version}\", \"aerospike/aerospike-server-${edition}-${short_platform}:latest\"]\n"
-		temp_str+="\t platforms=[\"${platform}\"]\n"
-		temp_str+="\t context=\"./${edition}/${distro}\"\n"
-		temp_str+="}\n\n"
-
-		g_test_targets_str+="${temp_str}"
-	done
-
-	printf -v platforms_str '%s,' "${platform_list[@]}"
-	platforms_str="${platforms_str%,}"
-
-	local target_str="${edition}_${distro}"
-	local temp_str="target \"${target_str}\" {\n"
-
-	local product="aerospike/aerospike-server"
-	local latest_version
-
-	if [ "${edition}" != "community" ]; then
-		product+="-${edition}"
-	fi
-
-	temp_str+="\t tags=[\"${product}:${server_version}\""
-
-	if [ -n "${g_container_release}" ]; then
-		temp_str+=", \"${product}:${server_version}_${g_container_release}\""
-	fi
-
-	latest_version="$(find_latest_server_version)"
-
-	if [ "${latest_version}" = "${server_version}" ]; then
-		temp_str+=", \"${product}:latest\""
-	fi
-
-	temp_str+="]\n"
-	temp_str+="\t platforms=[\"${platforms_str}\"]\n"
-	temp_str+="\t context=\"./${edition}/${distro}\"\n"
-	temp_str+="}\n\n"
-
-	g_push_targets_str+=${temp_str}
-}
-
 function copy_template() {
 	local template_path="template"
 	local target_path=$1
 
-	rm -rf "${target_path}"
+	if [ -d "${target_path}" ]; then
+		log_warn "unexpected - found '${target_path}'"
+	fi
+
 	mkdir -p "${target_path}"
 
 	for override in \
@@ -115,15 +26,6 @@ function copy_template() {
 			cp -r "${override_path}"/* "${target_path}"
 		fi
 	done
-}
-
-function bash_eval_templates() {
-	local target_path=$1
-
-	while IFS= read -r -d '' template_file; do
-		target_file="${template_file%.template}"
-		bash_eval_template "${template_file}" "${target_file}"
-	done < <(find "${target_path}" -type f -name "*.template" -print0)
 }
 
 function bash_eval_template() {
@@ -144,6 +46,55 @@ function bash_eval_template() {
 
 	# Ignore failure when template is mounted in a read-only filesystem.
 	rm "${template_file}" || true
+}
+
+function bash_eval_templates() {
+	local target_path=$1
+
+	while IFS= read -r -d '' template_file; do
+		target_file="${template_file%.template}"
+		bash_eval_template "${template_file}" "${target_file}"
+	done < <(find "${target_path}" -type f -name "*.template" -print0)
+}
+
+function do_template() {
+	local distro=$1
+	local edition=$2
+
+	log_info "do_template() - distro '${distro}' edition '${edition}'"
+
+	if [ -z "${g_tools_version}" ]; then
+		# Use the first lookup, the version should be the same for each.
+		g_tools_version=$(find_latest_tools_version_for_server "${distro}" "${edition}" "${g_server_version}")
+	fi
+
+	# These are variables used by the template.
+	DEBUG="${DEBUG:=false}"
+	LINUX_BASE="$(support_distro_to_base "${distro}")"
+	AEROSPIKE_VERSION="${g_server_version}"
+	CONTAINER_RELEASE="${g_container_release}"
+	AEROSPIKE_EDITION="${edition}"
+	AEROSPIKE_DESCRIPTION="Aerospike is a real-time database with predictable performance at petabyte scale with microsecond latency over billions of transactions."
+	AEROSPIKE_X86_64_LINK="$(get_package_link "${distro}" "${edition}" "${g_server_version}" "${g_tools_version}" "x86_64")"
+	AEROSPIKE_SHA_X86_64="$(fetch_package_sha "${distro}" "${edition}" "${g_server_version}" "${g_tools_version}" "x86_64")"
+	AEROSPIKE_AARCH64_LINK="$(get_package_link "${distro}" "${edition}" "${g_server_version}" "${g_tools_version}" "aarch64")"
+	AEROSPIKE_SHA_AARCH64="$(fetch_package_sha "${distro}" "${edition}" "${g_server_version}" "${g_tools_version}" "aarch64")"
+
+	log_info "DEBUG: '${DEBUG}'"
+	log_info "LINUX_BASE: '${LINUX_BASE}'"
+	log_info "AEROSPIKE_VERSION: '${AEROSPIKE_VERSION}'"
+	log_info "CONTAINER_RELEASE: '${CONTAINER_RELEASE}'"
+	log_info "AEROSPIKE_EDITION: '${AEROSPIKE_EDITION}'"
+	log_info "AEROSPIKE_DESCRIPTION: '${AEROSPIKE_DESCRIPTION}'"
+	log_info "AEROSPIKE_X86_64_LINK: '${AEROSPIKE_X86_64_LINK}'"
+	log_info "AEROSPIKE_SHA_X86_64: '${AEROSPIKE_SHA_X86_64}'"
+	log_info "AEROSPIKE_AARCH64_LINK: '${AEROSPIKE_AARCH64_LINK}'"
+	log_info "AEROSPIKE_SHA_AARCH64: '${AEROSPIKE_SHA_AARCH64}'"
+
+	local target_path="${edition}/${distro}"
+
+	copy_template "${target_path}"
+	bash_eval_templates "${target_path}"
 }
 
 function usage() {
@@ -208,73 +159,191 @@ function parse_args() {
 	shift $((OPTIND - 1))
 }
 
-function main() {
-	parse_args "$@"
-
-	if [ -z "${g_server_version}" ]; then
-		g_server_version=$(find_latest_server_version)
-	else
-		g_server_version=$(find_latest_server_version_for_lineage "${g_server_version}")
-	fi
-
-	local distros
-	local editions
+function generate_templates() {
 	local all_editions
-
-	IFS=' ' read -r -a distros <<<"$(support_distros_for_asd "${g_server_version}")"
-
-	if [ -z "${g_server_edition}" ]; then
-		IFS=' ' read -r -a editions <<<"$(support_editions_for_asd "${g_server_version}")"
-	else
-		editions=("${g_server_edition}")
-	fi
-
 	IFS=' ' read -r -a all_editions <<<"$(support_all_editions)"
 
+	# Clear prior builds.
 	for edition in "${all_editions[@]}"; do
 		find "${edition}"/* -maxdepth 0 -type d -exec rm -rf {} \;
 	done
 
-	local bake_hcl="bake.hcl"
-	g_test_group_str="#------------------------------------- test -----------------------------------\n\n"
-	local push_group_str="#------------------------------------ push -----------------------------------\n\n"
+	# Generate new builds.
+	for edition in "${g_editions[@]}"; do
+		for distro in "${g_distros[@]}"; do
+			do_template "${distro}" "${edition}"
+		done
+	done
+}
 
-	g_test_targets_str=
-	g_push_targets_str=
+function do_bake_test_group_targets() {
+	local distro=$1
+	local edition=$2
 
-	cat <<-EOF >"${bake_hcl}"
+	local platform_list
+	IFS=' ' read -r -a platform_list <<<"$(support_platforms_for_asd "${g_server_version}" "${edition}")"
+
+	local output=""
+
+	for platform in "${platform_list[@]}"; do
+		local short_platform=${platform#*/}
+		local target_str="${edition}_${distro}_${short_platform}"
+
+		output+="\"${target_str}\", "
+	done
+
+	printf "%s" "${output}"
+}
+
+function do_bake_group() {
+	local group=$1
+
+	local output="#------------------------------------ ${group} -----------------------------------\n\n"
+
+	output+="group \"${group}\" {\n\ttargets=["
+
+	for edition in "${g_editions[@]}"; do
+		for distro in "${g_distros[@]}"; do
+			if [[ "${group}" == "test" ]]; then
+				output+="$(do_bake_test_group_targets "${distro}" "${edition}")"
+			elif [[ "${group}" == "push" ]]; then
+				output+="\"${edition}_${distro}\", "
+			else
+				log_warn "unexpected group '%{group}'"
+				exit 1
+			fi
+		done
+	done
+
+	# (Optional) Trailing comma causes no problem to Docker Buildx Bake.
+	output=${output%,*}
+	output+="]\n}\n"
+
+	printf "%s" "${output}"
+}
+
+function do_bake_test_target() {
+	local distro=$1
+	local edition=$2
+
+	local platform_list
+	IFS=' ' read -r -a platform_list <<<"$(support_platforms_for_asd "${g_server_version}" "${edition}")"
+
+	local output=""
+
+	for platform in "${platform_list[@]}"; do
+		local short_platform=${platform#*/}
+		local target_str="${edition}_${distro}_${short_platform}"
+
+		output+="target \"${target_str}\" {\n"
+		output+="\t tags=[\"aerospike/aerospike-server-${edition}-${short_platform}:${g_server_version}\", \"aerospike/aerospike-server-${edition}-${short_platform}:latest\"]\n"
+		output+="\t platforms=[\"${platform}\"]\n"
+		output+="\t context=\"./${edition}/${distro}\"\n"
+		output+="}\n\n"
+	done
+
+	printf "%s" "${output}"
+}
+
+function do_bake_push_target() {
+	local distro=$1
+	local edition=$2
+
+	local platform_list
+	IFS=' ' read -r -a platform_list <<<"$(support_platforms_for_asd "${g_server_version}" "${edition}")"
+
+	printf -v platforms_str '%s,' "${platform_list[@]}"
+	platforms_str="${platforms_str%,}"
+
+	local target_str="${edition}_${distro}"
+	local output="target \"${target_str}\" {\n"
+
+	local product="aerospike/aerospike-server"
+
+	if [ "${edition}" != "community" ]; then
+		product+="-${edition}"
+	fi
+
+	output+="\t tags=[\"${product}:${g_server_version}\""
+
+	if [ -n "${g_container_release}" ]; then
+		output+=", \"${product}:${g_server_version}_${g_container_release}\""
+	fi
+
+	if [ "${g_latest_version}" = "${g_server_version}" ]; then
+		output+=", \"${product}:latest\""
+	fi
+
+	output+="]\n"
+	output+="\t platforms=[\"${platforms_str}\"]\n"
+	output+="\t context=\"./${edition}/${distro}\"\n"
+	output+="}\n\n"
+
+	printf "%s" "${output}"
+}
+
+function generate_bake_file() {
+	local bake_file="bake.hcl"
+
+	cat <<-EOF >"${bake_file}"
 		# This file contains the targets for the test images.
 		# This file is auto-generated by the update.sh script and will be wiped out by the update.sh script.
 		# Please don't edit this file.
 		#
 		# Build all test/push images:
-		#      docker buildx bake -f ${bake_hcl} [test | push] --progressive plain [--load | --push]
+		#      docker buildx bake -f ${bake_file} [test | push] --progressive plain [--load | --push]
 		# Build selected images:
-		#      docker buildx bake -f ${bake_hcl} [target name, ...] --progressive plain [--load | --push]
+		#      docker buildx bake -f ${bake_file} [target name, ...] --progressive plain [--load | --push]
 
 	EOF
 
-	g_test_group_str+="group \"test\" {\n\ttargets=["
-	push_group_str+="group \"push\" {\n\ttargets=["
+	local test_targets_str=""
+	local push_targets_str=""
 
-	for edition in "${editions[@]}"; do
-		for distro in "${distros[@]}"; do
-			run_template "${distro}" "${edition}"
-			push_group_str+="\"${edition}_${distro}\", "
+	for edition in "${g_editions[@]}"; do
+		for distro in "${g_distros[@]}"; do
+			test_targets_str+="$(do_bake_test_target "${distro}" "${edition}")"
+			push_targets_str+="$(do_bake_push_target "${distro}" "${edition}")"
 		done
 	done
 
-	# (Optional) Trailing comma causes no problem to Docker Buildx Bake.
-	g_test_group_str=${g_test_group_str%,*}
-	push_group_str=${push_group_str%,*}
-
-	g_test_group_str+="]\n}\n"
-	push_group_str+="]\n}\n"
+	local test_group_str
+	test_group_str="$(do_bake_group "test")"
+	local push_group_str
+	push_group_str="$(do_bake_group "push")"
 
 	{
-		printf "%b\n%b" "${g_test_group_str}" "${g_test_targets_str}"
-		printf "%b\n%b" "${push_group_str}" "${g_push_targets_str}"
-	} >>"${bake_hcl}"
+		printf "%b\n%b" "${test_group_str}" "${test_targets_str}"
+		printf "%b\n%b" "${push_group_str}" "${push_targets_str}"
+	} >>"${bake_file}"
+}
+
+function main() {
+	parse_args "$@"
+
+	g_latest_version=
+
+	if [ -z "${g_server_version}" ]; then
+		g_server_version=$(find_latest_server_version)
+		g_latest_version=g_server_version
+	else
+		g_server_version=$(find_latest_server_version_for_lineage "${g_server_version}")
+		g_latest_version=$(find_latest_server_version)
+	fi
+
+	g_distros=
+	IFS=' ' read -r -a g_distros <<<"$(support_distros_for_asd "${g_server_version}")"
+
+	g_editions=
+
+	if [ -z "${g_server_edition}" ]; then
+		IFS=' ' read -r -a g_editions <<<"$(support_editions_for_asd "${g_server_version}")"
+	else
+		g_editions=("${g_server_edition}")
+	fi
+
+	generate_templates
+	generate_bake_file
 }
 
 main "$@"
