78173dfa715ebe5fcc6bbbb373cce53d1ce9d817
Kevin Porter <kporter@aerospike.com>
2023-11-03 11:53:02 -0700

    Add cleanup for rpm builds - only leave two rpm builds for debugging.

--- data/config/6.1/config.sh
+++ data/config/6.1/config.sh
@@ -2,9 +2,9 @@
 
 export c_editions=("enterprise" "federal" "community")
 
-export c_distros=("debian11" "el8")
+export c_distros=("debian11")
 export c_distro_default="debian11"
-export c_distro_bases=("debian:bullseye-slim" "redhat/ubi8-minimal")
+export c_distro_bases=("debian:bullseye-slim")
 
 export c_archs=("x86_64")
 export c_platforms=("linux/amd64")
