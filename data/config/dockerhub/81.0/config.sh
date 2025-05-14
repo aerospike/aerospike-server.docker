ae781d845dccd911872e5561727e85e210137226
pvinh-spike <81987648+pvinh-spike@users.noreply.github.com>
2025-05-14 10:08:40 -0700

    Add config structure to support 80.1 & 81.0

---
+++ data/config/dockerhub/81.0/config.sh
@@ -0,0 +1,11 @@
+# shellcheck shell=bash
+
+export c_editions=("enterprise" "federal" "community")
+
+export c_distros=("ubuntu24.04")
+export c_distro_dir=("ubuntu24.04")
+export c_distro_default="ubuntu24.04"
+export c_distro_bases=("ubuntu:24.04")
+
+export c_archs=("x86_64" "aarch64")
+export c_platforms=("linux/amd64" "linux/arm64")
