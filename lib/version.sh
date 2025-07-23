8b82b5d3192e187c92584df2a73e0be71c9a04c8
pvinh-spike <81987648+pvinh-spike@users.noreply.github.com>
2025-07-22 20:17:56 -0700

    Update reg exp.

--- lib/version.sh
+++ lib/version.sh
@@ -6,7 +6,7 @@ source lib/fetch.sh
 
 ARTIFACTS_DOMAIN=${ARTIFACTS_DOMAIN:="https://artifacts.aerospike.com"}
 RE_VERSION='[0-9]+[.][0-9]+[.][0-9]+([.][0-9]+)+(-[a-z0-9]+)?([-][0-9]*[-]g[0-9a-z]*)?'
-GA_RE_VERSION='^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'
+GA_RE_VERSION='[0-9]+[.][0-9]+[.][0-9]+([.][0-9]+)+\/'
 
 function version_compare_gt() {
     v1=$1
@@ -39,7 +39,8 @@ function find_latest_ga_server_version() {
     # Note - we assume every release will have a enterprise component.
     server_version="$(
         fetch "${FUNCNAME[0]}" "${ARTIFACTS_DOMAIN}/aerospike-server-enterprise/" |
-            grep -oE "${GA_RE_VERSION}" |
+            grep -oE "${GA_RE_VERSION}" | 
+	    cut -d'/' -f1 |
             sort -V |
             tail -1
     )"
